c4de536dd92221c53b2f24da62a825915cb77e51
zlidzen <m.karsakou@gmail.com>
2023-10-01 18:04:40 +0200

    upgrade navigation for RFI 16_35

--- HardMTT/DefBB/3betPush16-25bb/navigation.js
+++ HardMTT/DefBB/3betPush16-25bb/navigation.js
@@ -4,7 +4,7 @@ const navHMTT = [
   {link:dip + 'RFI/Up36bb/Up36UTG.html', caption:'RFI 36bb+', isActive:false},
   {link:dip + 'RFI/16-35bb/16-35bbUTG.html', caption:'RFI 16-35bb', isActive:false},
   {link:dip + 'CC3bet/Up26bb/CC3betUp26bbUTG.html', caption:'CC/3bet 26bb+', isActive:false},
-  {link:dip + '16-35bb/CC3bet16-25bbUTG.html', caption:'CC/3bet 16-25bb', isActive:false},
+  {link:dip + 'CC3bet/16-35bb/CC3bet16-25bbUTG.html', caption:'CC/3bet 16-25bb', isActive:false},
   {link:dip + 'PushFold/PF-UTG.html', caption:'PUSH/FOLD', isActive:false},
   {link:dip + 'ISO/IsolateUTGMP.html', caption:'ISOLATE', isActive:false},
   {link:dip + 'SQUEEZE/SqueezeSBBBvsUTG.html', caption:'SQUEEZE', isActive:false},
