3f743ee1d4e6f33c42083f50cb850d2f0f682ffe
zlidzen <m.karsakou@gmail.com>
2023-10-01 18:18:34 +0200

    fix navigation to CC3bet 16_35

--- HardMTT/SQUEEZE/navigation.js
+++ HardMTT/SQUEEZE/navigation.js
@@ -4,7 +4,7 @@ const navHMTT = [
   {link:dip + 'RFI/Up36bb/Up36UTG.html', caption:'RFI 36bb+'},
   {link:dip + 'RFI/16-35bb/16-35bbUTG.html', caption:'RFI 16-35bb'},
   {link:dip + 'CC3bet/Up26bb/CC3betUp26bbUTG.html', caption:'CC/3bet 26bb+'},
-  {link:dip + '16-35bb/CC3bet16-25bbUTG.html', caption:'CC/3bet 16-25bb'},
+  {link:dip + 'CC3bet/16-35bb/CC3bet16-25bbUTG.html', caption:'CC/3bet 16-25bb'},
   {link:dip + 'PushFold/PF-UTG.html', caption:'PUSH/FOLD'},
   {link:dip + 'ISO/IsolateUTGMP.html', caption:'ISOLATE'},
   {link:null, caption:'SQUEEZE'},
